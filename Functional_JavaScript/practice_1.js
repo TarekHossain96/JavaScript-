7ce9d8abb7d298084848d722997f3fd4b10e6b17
TarekHossain96 <info.tarek96@gmail.com>
2019-03-03 23:21:59 +0600

    Functional Javascrit practice-1

--- Functional_JavaScript/practice_1.js
+++ Functional_JavaScript/practice_1.js
@@ -8,18 +8,18 @@ function sayName(name){
 
 // 2. === function can be stored in a Variable ====
 
-// var hello = sayName;
-// var fun = sayName('Tarek Hossain');
+var hello = sayName;
+var fun = sayName('Tarek Hossain');
 
-// console.log(hello);
-// console.log(fun);
+console.log(hello);
+console.log(fun);
 
 
 // 3. ==== A function can be stored an Array ===
 
-// var arr = [1,2,3];
-// arr.push(sayName)
-// console.log(arr);
+var arr = [1,2,3];
+arr.push(sayName)
+console.log(arr);
 
 // 4. === A function can be stored as an object field or property ====
 
@@ -47,3 +47,19 @@ var result = funName('Tarek Hossain', sayName);
 console.log(result);
 
 // 7. we can return function from another function
+
+function base(b){
+    return function (n) {
+        var result = 1;
+        for( var i=0; i<=b; i++){
+            result *=n
+        }
+        return result;
+    }
+}
+var power = base(2);
+var reslt =power(5);
+reslt;
+
+var result = base(2)(5);
+result
